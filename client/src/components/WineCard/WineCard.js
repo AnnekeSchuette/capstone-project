188c2af1a5fb01c9f3065e820e627998f8671ff5
Anneke Schütte <a.schuette.hh@gmail.com>
2021-03-11 10:36:42 +0100

    Create WineCard component file with initial parameters

---
+++ client/src/components/WineCard/WineCard.js
@@ -0,0 +1,10 @@
+import { wines } from '../../data/wine_recommendations_malbec.json'
+
+export default function WineCard({
+  title,
+  description,
+  imageUrl,
+  price,
+  averageRating,
+  ratingCount,
+}) {}
