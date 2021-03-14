5958fc79072b56774d2cea40b0bc38e4835e90d7
Anneke Schütte <a.schuette.hh@gmail.com>
2021-03-14 18:38:15 +0100

    Proxy setup fix (remove brackets in require)

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,8 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const createProxyMiddleware = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-    '/api',
+    '/client',
     createProxyMiddleware({
       target: 'http://localhost:5000',
       changeOrigin: true,
