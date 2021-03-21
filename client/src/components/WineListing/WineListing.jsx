97497da5aabf319ea9cee33440d0e47a5d0233dc
Anneke Schütte <a.schuette.hh@gmail.com>
2021-03-21 23:49:30 +0100

    Fix render issues regarding diverse data in results

--- client/src/components/WineListing/WineListing.jsx
+++ client/src/components/WineListing/WineListing.jsx
@@ -2,54 +2,55 @@ import WineCard from 'components/WineCard/WineCard'
 import styled from 'styled-components/macro'
 
 export default function WineListing({ results, savedWines, onFavToggle }) {
-  const wines =
-    results.productMatches.length > 0 ? (
-      results.productMatches.map(
-        ({
-          id,
-          title,
-          description,
-          imageUrl,
-          price,
-          averageRating,
-          ratingCount,
-          score,
-          link,
-        }) => (
-          <WineCard
-            id={id}
-            key={id}
-            title={title}
-            description={description}
-            imageUrl={imageUrl}
-            price={price}
-            averageRating={averageRating}
-            ratingCount={ratingCount}
-            score={score}
-            link={link}
-            savedWines={savedWines}
-            onFavToggle={onFavToggle}
-          />
-        )
+  const wines = results.productMatches ? (
+    results.productMatches.map(
+      ({
+        id,
+        title,
+        description,
+        imageUrl,
+        price,
+        averageRating,
+        ratingCount,
+        score,
+        link,
+      }) => (
+        <WineCard
+          id={id}
+          key={id}
+          title={title}
+          description={description}
+          imageUrl={imageUrl}
+          price={price}
+          averageRating={averageRating}
+          ratingCount={ratingCount}
+          score={score}
+          link={link}
+          savedWines={savedWines}
+          onFavToggle={onFavToggle}
+        />
       )
-    ) : (
-      <ListEmptyMessage>No results. 😢</ListEmptyMessage>
     )
+  ) : (
+    <ListEmptyMessage>No results. 😢</ListEmptyMessage>
+  )
 
   return (
     <>
       <WineList>
         {wines}
-        <p>{results.pairingText}</p>
+        {results.pairingText !== '' && <p>{results.pairingText}</p>}
 
-        <MoreInfoWrap>
-          Matching types of wine:
-          <BadgeList>
-            {results.pairedWines.map(wine => (
-              <Badge>{wine}</Badge>
-            ))}
-          </BadgeList>
-        </MoreInfoWrap>
+        {results.pairedWines.length > 0 && (
+          <MoreInfoWrap>
+            Matching types of wine:
+            <BadgeList>
+              {results.pairedWines.map(wine => (
+                <Badge>{wine}</Badge>
+              ))}
+            </BadgeList>
+          </MoreInfoWrap>
+        )}
       </WineList>
     </>
   )
@@ -78,7 +79,7 @@ const Badge = styled.li`
   list-style: none;
   padding: 2px 5px;
   margin: 0;
-  background: var(--color-complementary);
+  background: var(--color-candy-pink);
   border-radius: 5px;
   color: var(--color-midnight);
   font-size: 0.8em;
