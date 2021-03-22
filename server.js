55dcf3ea9150e0ce9107e18e0822e9e096019437
Anneke Schütte <a.schuette.hh@gmail.com>
2021-03-22 21:32:10 +0100

    Heroku compliing fix: Move fonts up to root

--- server.js
+++ server.js
@@ -12,13 +12,6 @@ app.use('/api/categories', require('./routes/categories'))
 app.use('/api/stored-wines', require('./routes/storedWines'))
 app.use(require('./routes/error'))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-  })
-}
-
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`)
 })
