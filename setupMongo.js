7a05b2143b13de57bb5316b209653c1797da2050
Anneke Schütte <a.schuette.hh@gmail.com>
2021-03-23 11:19:09 +0100

    Prefix MonDBG Atlas env var

--- setupMongo.js
+++ setupMongo.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 require('dotenv').config()
-const { ATLAS_URL } = process.env
+const { REACT_APP_MONGODB_ATLAS_URL } = process.env
 
 module.exports = function setupMongo() {
   mongoose
-    .connect(ATLAS_URL, {
+    .connect(REACT_APP_MONGODB_ATLAS_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
