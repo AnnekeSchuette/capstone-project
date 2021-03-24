b184a2f1ae535e03098fc38413d060ac5bab0efb
Anneke Schütte <a.schuette.hh@gmail.com>
2021-03-24 21:46:36 +0100

    Adapt test for WineListing

--- client/src/components/WineListing/WineListing.spec.js
+++ client/src/components/WineListing/WineListing.spec.js
@@ -1,14 +1,8 @@
 import { screen, render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import WineListing from 'components/WineListing/WineListing'
-import { recommendedWines } from 'data/wine_recommendations_malbec.json'
 import { createMemoryHistory } from 'history'
 import { Router } from 'react-router'
-
-const history = createMemoryHistory()
-history.push = jest.fn()
-
-const wrapper = ({ children }) => <Router history={history}>{children}</Router>
 
 const testdata = {
   pairedWines: ['bordeaux', 'champagne', 'white burgundy'],
@@ -28,19 +22,44 @@ const testdata = {
       link:
         'https://click.linksynergy.com/deeplink?id=*QCiIS6t4gA&mid=2025&murl=https%3A%2F%2Fwww.wine.com%2Fproduct%2Fchateau-pindefleurs-st-emilion-bordeaux-2009%2F125798',
     },
+    {
+      id: 443696,
+      title: 'Baron des Chartrons Bordeaux',
+      description:
+        'Nose of fresh red fruits. Great structure, soft tannins, and fresh finale of ripe cherry and blueberry. Elegant and very typical of the appellation.Baron des Chartrons Rouge is ideally paired with grilled beef, cheese pastas or lamb.',
+      price: '$10.99',
+      imageUrl: 'https://spoonacular.com/productImages/443696-312x231.jpg',
+      averageRating: 0.9400000000000001,
+      ratingCount: 5,
+      score: 0.8775000000000001,
+      link:
+        'https://click.linksynergy.com/deeplink?id=*QCiIS6t4gA&mid=2025&murl=https%3A%2F%2Fwww.wine.com%2Fproduct%2Fbaron-des-chartrons-bordeaux-2015%2F164949',
+    },
   ],
 }
 
+const history = createMemoryHistory()
+history.push = jest.fn()
+
+const wrapper = ({ children }) => <Router history={history}>{children}</Router>
+
 describe('WineListing', () => {
-  it('renders a child component, with the title "Chateau Pindefleurs St-Emilion Bordeaux"', () => {
+  it('renders all productMatches in the list, which should be 2 WineCards in this case', () => {
+    render(<WineListing results={testdata} />)
+    expect(screen.queryAllByRole('heading', { level: 3 })).toHaveLength(2)
+  })
+
+  it('renders a "pairingText"', () => {
     render(<WineListing results={testdata} />, { wrapper })
     expect(
-      screen.queryByText('Chateau Pindefleurs St-Emilion Bordeaux')
+      screen.queryByText(
+        /Souffle can be paired with Bordeaux, Champagne, and White Burgundy./i
+      )
     ).toBeInTheDocument()
   })
 
-  it('renders a list of 15 WineCards', () => {
-    render(<WineListing results={recommendedWines} />)
-    expect(screen.queryAllByRole('heading', { level: 3 })).toHaveLength(15)
+  it('renders "pairedWines" as list', () => {
+    render(<WineListing results={testdata} />, { wrapper })
+    expect(screen.getAllByRole('listitem')).toHaveLength(3)
   })
 })
