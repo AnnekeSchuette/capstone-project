0f01b1d68640da98646f8d8066784a7d346afd98
Anneke Schütte <a.schuette.hh@gmail.com>
2021-03-27 21:59:37 +0100

    Add missing body to PATCH function

--- client/src/services/editJournalEntry.js
+++ client/src/services/editJournalEntry.js
@@ -1,9 +1,10 @@
-export default function editJournalEntry(id) {
+export default function editJournalEntry(id, rating, notes) {
   return fetch(`/api/journal-entries/${id}/edit`, {
     method: 'PATCH',
     headers: {
       'content-type': 'application/json',
     },
+    body: JSON.stringify({ rating, notes }),
   })
     .then(res => res.json())
     .then(data => (data.error ? Promise.reject(data) : data))
