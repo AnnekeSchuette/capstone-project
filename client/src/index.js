a1a1cf90caa84776dabe8dcbd72e459b45a9d390
Anneke Schütte <a.schuette.hh@gmail.com>
2021-03-30 11:07:57 +0200

    Edit app meta data

--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,8 @@ import GlobalFonts from './fonts/Fonts'
 import App from 'components/App/App'
 import GlobalStyle from 'components/GlobalStyle'
 import { BrowserRouter as Router } from 'react-router-dom'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
 
 const queryClient = new QueryClient()
 
@@ -28,3 +30,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
