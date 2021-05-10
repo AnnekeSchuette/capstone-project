0f71a807032d37775d0d96f32693f7b56d5d5e16
Anneke Schütte <a.schuette.hh@gmail.com>
2021-05-10 20:14:07 +0200

    Add JournalForm to Storybook

---
+++ client/src/components/JournalForm/JournalForm.stories.js
@@ -0,0 +1,18 @@
+import JournalForm from 'components/JournalForm/JournalForm'
+import { actions } from '@storybook/addon-actions'
+
+export default {
+  title: 'Capstone Project/JournalForm',
+  component: JournalForm,
+}
+
+const Template = args => <JournalForm {...args} {...events} />
+const events = actions({ setSearch: 'saved!' })
+
+export const Default = Template.bind({})
+Default.args = {
+  ratingScore: 3,
+  journalContent:
+    'This is a very tasty wine from Jaques. Goes perfectly with Steak!',
+  editMode: true,
+}
